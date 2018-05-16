a4e0d6224212705ab725c4051338993e76c887c1
CJT1105 <CJT1105@GMAIL.COM>
2018-05-16 00:34:25 -0500

    set up user registration step defenitions

--- tests/features/support/user-registration-steps.js
+++ tests/features/support/user-registration-steps.js
@@ -1,3 +1,20 @@
-module.exports = () => {
-    
+module.exports = function() {
+    this.Given('the user is on the registration page', function () {
+        browser.url('http://localhost:3000/register')
+    });
+
+    this.When('the user inputs an email address', function () {
+        // Write code here that turns the phrase above into concrete actions
+        return 'pending';
+    });
+
+    this.When('the user inputs a password', function () {
+        // Write code here that turns the phrase above into concrete actions
+        return 'pending';
+    });
+
+    this.When('the user selects a university', function () {
+        // Write code here that turns the phrase above into concrete actions
+        return 'pending';
+    });
 }
